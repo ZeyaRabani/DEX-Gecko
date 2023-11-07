fa470692b61ca72b58d31ec029a3621cd59ebb64
ZeyaRabani <ziyarabani@hotmail.co.uk>
2023-11-07 16:02:39 +0000

    Changed the name, C10

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,9 @@ const Navbar = () => {
     return (
         <Link to='/'>
             <div className='navbar'>
-                <FaCoins className='icon' />
-                <h1> Coin <span className='purple'>Search</span></h1>
+
+                <h1> <span className='purple'> Zeya Rabani</span></h1>
+                <h2>C10</h2>
             </div>
         </Link>
     )
